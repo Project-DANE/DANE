72853d14925c2610ecfcc0b383b84ffea7237ffa
Andrew Konstans <andrew.konstans10@gmail.com>
2023-03-16 10:52:04 -0500

    start

--- wrangle.py
+++ wrangle.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np 
-
-import 
-
 def get_cws_data(): 
     
     
@@ -85,3 +82,37 @@ def train_vailidate_test_split(df, target, strat = None):
     
     return train, validate, test, X_train, y_train, X_val, y_val, X_test, y_test
 
+def scale_splits(X_train, X_val, X_test, scaler, columns = False):
+    '''
+    Accepts input of a train validate test split and a specific scaler. The function will then scale
+    the data according to the scaler used and output the splits as scaled splits
+    If you want to scale by specific columns enter them in brackets and quotations after entering scaler
+    otherwise the function will scale the entire dataframe
+    '''
+    if columns:
+        scale = scaler.fit(X_train[columns])
+        train_initial = pd.DataFrame(scale.transform(X_train[columns]),
+        columns= X_train[columns].columns.values).set_index([X_train.index.values])
+        val_initial = pd.DataFrame(scale.transform(X_val[columns]),
+        columns= X_val[columns].columns.values).set_index([X_val.index.values])
+        test_initial = pd.DataFrame(scale.transform(X_test[columns]),
+        columns= X_test[columns].columns.values).set_index([X_test.index.values])
+        train_scaled = X_train.copy()
+        val_scaled = X_val.copy()
+        test_scaled = X_test.copy()
+        train_scaled.update(train_initial)
+        val_scaled.update(val_initial)
+        test_scaled.update(test_initial)
+
+    else:
+        scale = scaler.fit(X_train)
+        train_scaled = pd.DataFrame(scale.transform(X_train),
+        columns= X_train.columns.values).set_index([X_train.index.values])
+        val_scaled = pd.DataFrame(scale.transform(X_val),
+        columns= X_val.columns.values).set_index([X_val.index.values])
+        test_scaled = pd.DataFrame(scale.transform(X_test),
+        columns= X_test.columns.values).set_index([X_test.index.values])
+        train_scaled = X_train.copy()
+        val_scaled = X_val.copy()
+        test_scaled = X_test.copy()
+    return train_scaled, val_scaled, test_scaled
