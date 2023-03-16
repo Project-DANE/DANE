1141d07d172a9c8e5ecd8a2e6584480b93c474c5
Andrew Konstans <andrew.konstans10@gmail.com>
2023-03-16 10:44:58 -0500

    origin

--- prepare.py
+++ prepare.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+<<<<<<< HEAD
 df = pd.read_csv('cws_residents.csv')
 # Pull CSV that has been queried from CWS database
 
@@ -8,11 +9,25 @@ df = df.drop(columns = ['HMY', 'HMYPerson', 'Rent', 'SRENT', 'hTenant', 'hTent',
 # Drop repetative columns that contained same inofrmation
 
 df = df.rename(columns = {'HPerson': 'id',
+=======
+
+def prep(df):
+
+    '''takes a dataframe, drops repetative columns, filters for current status, resets index and returns
+    a dataframe'''
+    
+    # Drop repetative columns that contained same inofrmation
+    df = df.drop(columns = ['HMY', 'HMYPerson', 'Rent', 'SRENT', 'hTenant', 'hTent', 'HMY1'])
+    
+    # Rename columns into a pythonic format
+    df = df.rename(columns = {'HPerson': 'id',
+>>>>>>> origin/main
                    'STOTALAMOUNT': 'total_charges',
                    'SAmountPaid': 'amount_paid',
                    'BOPEN': 'open',
                    'SNOTES': 'description',
                    'HRetentionacct': 'charge_code',
+<<<<<<< HEAD
                    'HProperty': 'prop_id',
                    'SNAME':'charge_name',
                    'cRent': 'rent',
@@ -29,3 +44,26 @@ df = df[df.sStatus == 'Current']
 
 df = df.reset_index(drop=True)
 # Reset the index to account for duplicates dropped
+=======
+                   'HProperty': 'property_id',
+                   'sStatus': 'status',
+                   'SNAME':'charge_name',
+                   'cRent': 'rent',
+                   'iTerm': 'term',
+                   'dIncome': 'monthly_income',
+                   'GuarantorRequired': 'guarrantor_required',
+                   'TotalIncome': 'total_income',
+                   'Recommendation': 'recommendation',
+                   'AverageApplicantAge': 'age',
+                   'AvgRiskScore':'risk_score',
+                   'ReasonThatDroveDecisionDescription': 'reason'})
+
+    # Eliminate duplicate charges by citing only current leases
+    df = df[df.status == 'Current']
+    
+    # Reset the index to account for duplicates dropped
+    df = df.reset_index(drop=True)
+    
+    # return a dataframe
+    return df
+>>>>>>> origin/main
