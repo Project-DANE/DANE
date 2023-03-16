25991483609cc3efceca2d0632f8a3cdddf86d96
Nicolas Angarita <nicolas.angarita07@gmail.com>
2023-03-16 11:16:31 -0500

    updates

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-import 
 
 def get_cws_data(): 
     
